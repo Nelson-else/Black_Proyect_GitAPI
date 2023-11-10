fa8d3675c5afaba3a83d934321ff6437e3b5a1da
Nelson Largaespada <largaespadanelson@gmail.com>
2023-11-09 22:46:01 -0600

    route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   resources :estudiantes
   #root 'estudiantes#index'
-  get '/github/repositories', to: 'github#repositories'
+
   root 'github#repositories'
+ 
+  get '/github/repositories', to: 'github#repositories'
+  
+  get '/github/download_repository', to: 'github#download_repository', as: 'download_repository'
+  # Otros enrutamientos de tu aplicación...
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
