b953ec83421a8c844e3395469cebd94b2c28004d
Nelson Largaespada <largaespadanelson@gmail.com>
2023-11-09 22:17:20 -0600

    view
    New view para mostrar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :estudiantes
   root 'estudiantes#index'
+  get '/github/repositories', to: 'github#repositories'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
